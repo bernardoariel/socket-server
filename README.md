fd0e360b75eaa5a333eadf2c95a73371a3424450
Noelia Bernardo <nollybernardo@gmail.com>
2023-02-17 22:36:32 -0300

    Primer commit del Server

---
+++ README.md
@@ -0,0 +1,15 @@
+# Socket-Server
+
+reconstruir módulos de Node
+``````
+npm install
+``````
+Generar el DIST
+``````
+tsc -w
+``````
+Levantar servidor, cualquiera de estos dos comandos
+``````
+nodemon dist/
+node dist/
+``````
---
+++ routes/routes.ts
@@ -0,0 +1,32 @@
+import { Router, Request, Response } from 'express'
+
+export const router = Router();
+
+router.get('/mensajes', (req:Request, resp: Response)=>{
+    resp.json({
+        ok: true,
+        mensaje: 'Todo está bien!!'
+    })
+})
+router.post('/mensajes', (req:Request, resp: Response)=>{
+
+    const cuerpo = req.body.cuerpo
+    const de = req.body.de
+    resp.json({
+        ok: true,
+        cuerpo:cuerpo,
+        de
+    })
+})
+router.post('/mensajes/:id', (req:Request, resp: Response)=>{
+
+    const cuerpo = req.body.cuerpo
+    const de = req.body.de
+    const id = req.params.id;
+    resp.json({
+        ok: true,
+        cuerpo:cuerpo, 
+        de, 
+        id
+    })
+})
